03ed986ab8fb5efa60b09c07af74a03b9279657d
samuelalanjohnson <samuelalanjohnson@mailfence.com>
2025-05-26 21:28:11 -0500

    Update comp.sh
    removed gdb option from the gcc command

--- comp.sh
+++ comp.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 # little shell script to compile the program forgot how to use make lolz
-gcc -std=c99 -ggdb -c -g ./src/*.c
+gcc -std=c99 -c -g ./src/*.c
 
 gcc -std=c99 ./*.o -o calc
 
